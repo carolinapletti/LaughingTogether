b8df5ffb837bec4bcaf3d402914edc12f4b61710
carolinapletti <carolina.pletti@gmail.com>
2024-10-18 14:33:40 +0200

    [update]: modified WTC analyses: a different toolbox (the one already included in Matlab) is used to calculate WTC. Saved data include WTC matrixes with one value per time and period, WTC vectors with one vector per period averaged through time, and WTC averages through both time and period.

--- scripts/functions/LT_WTC.m
+++ scripts/functions/LT_WTC.m
@@ -61,10 +61,17 @@ function [cfg] = LT_WTC(cfg)
             %calculate ROIs
             hbo_1 = data_sub1.hbo{m};
             badChannels_1 = data_sub1.badChannels{m};
-            t_1 = data_sub1.t{m};
+            t = data_sub1.t{m};
+            fs = data_sub1.fs;
 
             hbo_2 = data_sub2.hbo{m};
             badChannels_2 = data_sub2.badChannels{m};
+            %the time vectors for both participants should be identical.
+            %check if that's the case
+            if data_sub2.t{m} ~= t
+                fprintf('the time vectors of the two participants don''t correspond!')
+                return;
+            end
             if ROI == 1
                 %average all channels by ROI
                 [hbo_1, badChannels_1] = calcROI(hbo_1, badChannels_1);
@@ -74,7 +81,7 @@ function [cfg] = LT_WTC(cfg)
             %calculate coherences
 
             try
-                coherences_all{m} = calcCoherence(hbo_1, hbo_2, t_1, badChannels_1, badChannels_2);
+                coherences_all{m} = calcCoherence(hbo_1, hbo_2, badChannels_1, badChannels_2, t, fs);
             catch exception
                 fprintf('couldnt calculate coherence for this part!\n');
                 msgText = getReport(exception);
--- scripts/functions/calcCoherence.m
+++ scripts/functions/calcCoherence.m
@@ -1,17 +1,18 @@
-function [coherences] = calcCoherence(hboSub1, hboSub2, t_1, badChannels_1, badChannels_2)   
-    % Calc the period 
+function [coherences] = calcCoherence(hbo_1, hbo_2, badChannels_1, badChannels_2, t, fs)   
+    % Calc the period of interest
     error = 0;
-    max = round(length(t_1) / 7.8/4); %trial duration/4
+    max = round(length(t) / fs/4); %trial duration/4
+    ts = 1/fs;
     poi=[8 max]; %limits period of interest from greater than 4 times the filter to smaller than trial duration/4
-    poi_index = zeros(2,1); %in which columns does the perios of interest starts/ends?
+    poi_index = zeros(2,1); %in which columns does the perios of interest starts/ends?    
+    sigPart1 = hbo_1(:,1);
+    sigPart2 = hbo_2(:,1);
     
-    sigPart1 = [t_1, hboSub1(:,1)];
-    sigPart2 = [t_1, hboSub2(:,1)]; %just use the time stamps of the first participant for both, since the two time vectors need to be the same
 
     try    
-        [~,period,~,~,~] = CP_mod_wtc(sigPart1, sigPart2); %already calculates wtc and extracts period (that is, all "frequences" that are calculated)
-        poi_index(1) = find(period > poi(1), 1, 'first'); %finds the first column in period which is greater than the maximum period of interest
-        poi_index(2) = find(period < poi(2), 1, 'last'); %finds the last column in period which is lower than the minimum period of interest
+        [wcoh,wcs,period] = wcoherence(sigPart1,sigPart2,seconds(ts)); %already calculates wtc and extracts period (that is, all "frequences" that are calculated)
+        poi_index(1) = find(period > seconds(poi(1)), 1, 'first'); %finds the first column in period which is greater than the maximum period of interest
+        poi_index(2) = find(period < seconds(poi(2)), 1, 'last'); %finds the last column in period which is lower than the minimum period of interest
     catch exception
         error = 1;
         fprintf('<strong>Impossible to calculate period for some reason. Trial too short?</strong>\n');
@@ -29,19 +30,19 @@ function [coherences] = calcCoherence(hboSub1, hboSub2, t_1, badChannels_1, badC
         % -------------------------------------------------------------------------
         %calculate how many combinations of channels there are (e.g. rTPJ1 x
         %rTPJ2, lTPJ1 x rTPJ2, etc)
-        numOfChan = size(hboSub1, 2)*size(hboSub2, 2);
+        numOfChan = size(hbo_1, 2)*size(hbo_2, 2);
         %create 1xnumOfChan cell of cells. this will contain all the final
         %values for this participant pair
         coherences{numOfChan}  = []; 
         %fill each subcell with NaNs. The size is: one row per period of
         %interest, one column per time point
-        coherences(:,:) = {NaN(poi_index(2)-poi_index(1)+1, length(hboSub1)+2)};
+        coherences(:,:) = {NaN(poi_index(2)-poi_index(1)+1, length(hbo_1)+3)};  %+ 3 columns because the first one is for the period, the second for channel number sub 1, the third for channel number sub 2
         
         %this variable contains the wavelet transform coherence values
         %calculated for each combination of channel and the content changes
         %for every iteration of the loop 
         Rsq{numOfChan} = [];
-        Rsq(:) = {NaN(length(period), length(t_1))};
+        Rsq(:) = {NaN(length(period), length(t))};
 
         % -------------------------------------------------------------------------
         % Calculate Coherence increase between conditions for every channel of the 
@@ -67,16 +68,16 @@ function [coherences] = calcCoherence(hboSub1, hboSub2, t_1, badChannels_1, badC
                 Ch_Sub2 = 1;
             end
             %the first column contains the periods
-            coherences{i}(:,1)  = period(poi_index(1):poi_index(2))';
+            coherences{i}(:,1)  = seconds(period(poi_index(1):poi_index(2)));
             %the second contains the channel number of subject 1
             coherences{i}(:,2)  = repelem(Ch_Sub1, length(coherences{i}(:,2)));
             %the third contains the channel number of subject 2
             coherences{i}(:,3)  = repelem(Ch_Sub2, length(coherences{i}(:,2)));
             if ~any(badChannels_1 == Ch_Sub1) && ~any(badChannels_2 == Ch_Sub2) % check if the channels were not rejected in one of the two subjects during preprocessing
-                sigPart1 = [t_1, hboSub1(:,Ch_Sub1)];
-                sigPart2 = [t_1, hboSub2(:,Ch_Sub2)]; %artificially set the time of the second participants to t_1
+                sigPart1 = hbo_1(:,Ch_Sub1);
+                sigPart2 = hbo_2(:,Ch_Sub2);
                 try
-                    [Rsq{i}, ~, ~, coi, ~] = CP_mod_wtc(sigPart1, sigPart2);                % r square - measure for coherence
+                    [Rsq{i}, wcs, period2, coi] =wcoherence(sigPart1,sigPart2,seconds(ts));                 % r square - measure for coherence
                 catch exception
                     msgText = getReport(exception);
                     fprintf(msgText);
